24ea87c5d7e680bcebac46e6daae1c65af83bb77
LSAfrika <lstudiosafrika@gmail.com>
2022-12-04 14:28:11 +0300

    still working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lstudiosafrika';
+
+
+  projects=[
+{
+  title:'ng chat',
+  bgimage:'../assets/unsplash/daniel-korpai-r73OFSry5AI-unsplash.jpg'
+},{
+  title:'ng snapshare',
+  bgimage:'../assets/unsplash/daniel-korpai-r73OFSry5AI-unsplash.jpg'
+},{
+  title:'ng check list',
+  bgimage:'../assets/unsplash/kelly-sikkema--1_RZL8BGBM-unsplash.jpg'
+},{
+  title:'ng gallery',
+  bgimage:'../assets/unsplash/annie-spratt-EvBPSTn-p5U-unsplash.jpg'
+},
+
+  ]
 }
