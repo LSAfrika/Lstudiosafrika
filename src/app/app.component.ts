b5ec232948498498cd5a4c3d3f9972911dce316a
LSAfrika <lstudiosafrika@gmail.com>
2022-12-05 15:22:38 +0300

    finished working on mobile nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lstudiosafrika';
+  open = false
 
-
-  projects=[
-{
-  title:'ng chat',
-  bgimage:'../assets/unsplash/daniel-korpai-r73OFSry5AI-unsplash.jpg'
-},{
-  title:'ng snapshare',
-  bgimage:'../assets/unsplash/daniel-korpai-r73OFSry5AI-unsplash.jpg'
-},{
-  title:'ng check list',
-  bgimage:'../assets/unsplash/kelly-sikkema--1_RZL8BGBM-unsplash.jpg'
-},{
-  title:'ng gallery',
-  bgimage:'../assets/unsplash/annie-spratt-EvBPSTn-p5U-unsplash.jpg'
-},
+  projects = [
+    {
+      title: 'ng chat',
+      bgimage: '../assets/unsplash/daniel-korpai-r73OFSry5AI-unsplash.jpg'
+    }, {
+      title: 'ng snapshare',
+      bgimage: '../assets/unsplash/daniel-korpai-r73OFSry5AI-unsplash.jpg'
+    }, {
+      title: 'ng check list',
+      bgimage: '../assets/unsplash/kelly-sikkema--1_RZL8BGBM-unsplash.jpg'
+    }, {
+      title: 'ng gallery',
+      bgimage: '../assets/unsplash/annie-spratt-EvBPSTn-p5U-unsplash.jpg'
+    },
 
   ]
+
+
+  togglemenu() {
+    this.open = !this.open
+    console.log('bool value: ', this.open);
+
+  }
 }
