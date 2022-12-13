96964d256cf1d2a08401f9263607e12dd7a269b2
LSAfrika <lstudiosafrika@gmail.com>
2022-12-13 17:41:41 +0300

    animation menu and resize desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lstudiosafrika';
-  open = false
-  mobiledev=false;
-switchtitle =1
+  open = 0
+  mobiledev = false;
+  switchtitle = 1
+  selection = 1
   projects = [
     {
       title: 'ng chat',
@@ -27,35 +28,56 @@ switchtitle =1
 
   ]
 
-  ngOnInit(){
-  
-      this.switch()
+  constructor() {
+
+  }
+  ngOnInit() {
+
+    this.switch()
 
   }
 
   togglemenu() {
-    this.open = !this.open
     console.log('bool value: ', this.open);
+    if (this.open === 1) return this.open = 2
+    if (this.open === 2) return this.open = 1
+    if (this.open === 0) return this.open = 1
 
   }
 
-  switch(){
-  
-    setTimeout(() => {
-      
-      
-    console.log('before update: ',this.switchtitle);
+  switch() {
 
-    if(this.switchtitle===2){this.mobiledev=true}
-    if(this.switchtitle===4) {this.switchtitle=0
-    }
-    this.switchtitle++
-    console.log('after update: ',this.switchtitle);
-    this.switch() 
+    setTimeout(() => {
+
+
+      // console.log('before update: ', this.switchtitle);
+
+      if (this.switchtitle === 2) { this.mobiledev = true }
+      if (this.switchtitle === 4) {
+        this.switchtitle = 0
+      }
+      this.switchtitle++
+      // console.log('after update: ', this.switchtitle);
+      this.switch()
     }, 3000);
 
-    
+
   }
 
+
+  scrolltocontent(location: string) {
+    // this.scroller.scrollToAnchor("contacts");
+    if (location === 'home') this.selection = 1
+    if (location === 'about') this.selection = 2
+    if (location === 'services') this.selection = 3
+    if (location === 'portfolio') this.selection = 4
+
+
+    document.getElementById(location).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
